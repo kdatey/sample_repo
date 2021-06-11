ed4636d21093eb8de26d256583e2feb3116df9b4
kdatey <36696401+kdatey@users.noreply.github.com>
2021-06-11 06:23:58 +0000

    Update another_script.py

--- another_script.py
+++ another_script.py
@@ -1,9 +1,8 @@
-from datetime import datetime
-import re
-import csv
-
-
 class Telephone:
+    
+    def __init__(subno, imsi):
+        self.subno = subno
+        self.imsi = imsi
 
     def switch_on(self, pwr, **options):
         pass
